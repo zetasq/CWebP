0e43cc72dd08dc2a950a1574587be751797f75a5
Zhu Shengqi <zetasq@gmail.com>
2018-03-29 21:28:50 +0800

    Initial Commit

---
+++ CWebP/CWebP.h
@@ -0,0 +1,19 @@
+//
+//  CWebP.h
+//  CWebP
+//
+//  Created by Zhu Shengqi on 29/03/2018.
+//  Copyright © 2018 Zhu Shengqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CWebP.
+FOUNDATION_EXPORT double CWebPVersionNumber;
+
+//! Project version string for CWebP.
+FOUNDATION_EXPORT const unsigned char CWebPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CWebP/PublicHeader.h>
+
+
